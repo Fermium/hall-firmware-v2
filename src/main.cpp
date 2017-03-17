0d9834345cad9ef75660bf7cc7a25ba2f57b5fa8
Davide Bortolami <d@fermiumlabs.com>
2017-03-17 17:29:20 +0100

    wip

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+#include <Arduino.h>
+#include <Wire.h>
+
+void setup() {
+
+	delay(1000);
+
+}
+
+void loop() {
+
+	digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(1000);                       // wait for a second
+  digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000);   
+
+}
